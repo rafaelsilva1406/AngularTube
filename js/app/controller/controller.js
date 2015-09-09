135f9ed13d20691a94f852e8e5fe3b6e3fbb31ef
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2015-09-08 17:45:11 -0700

    Startup files for AngularTube

---
+++ js/app/controller/controller.js
@@ -0,0 +1,66 @@
+﻿window.angularTube.controller = (function () {
+
+    function init(obj) {
+        obj.controller('VideosController', function ($scope, $http, $log, VideosService,promiseTracker) {
+            var $promise = {};
+            configService();
+
+            function configService() {
+                $scope.youtube = VideosService.getYoutube();
+                $scope.results = VideosService.getResults();
+                $scope.upcoming = VideosService.getUpcoming();
+                $scope.history = VideosService.getHistory();
+                $scope.playlist = true;
+                $scope.progress = promiseTracker();
+            }
+
+            $scope.launch = function (id, title) {
+                VideosService.launchPlayer(id, title);
+                VideosService.archiveVideo(id, title);
+                VideosService.deleteVideo($scope.upcoming, id);
+                $log.info('Launched id:' + id + ' and title:' + title);
+            };
+
+            $scope.queue = function (id, title) {
+                VideosService.queueVideo(id, title);
+                VideosService.deleteVideo($scope.history, id);
+                $log.info('Queued id:' + id + ' and title:' + title);
+            };
+
+            $scope.delete = function (id) {
+                VideosService.deleteVideo($scope.upcoming, id);
+            };
+
+            $scope.submit = function () {
+                $promise = $http.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        key: 'AIzaSyB49USoWlwgKupg7BjR-6EPQGftTbk9xEo',
+                        type: 'video',
+                        maxResults: '10',
+                        part: 'id,snippet',
+                        fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle',
+                        q: this.query
+                    }
+                })
+                .success(function (data) {
+                    VideosService.listResults(data);
+                    $log.info(data);
+                })
+                .error(function () {
+                    $log.info('Search error');
+                });
+
+                $scope.progress.addPromise($promise);
+            }
+
+            $scope.tabulate = function (state) {
+                $scope.playlist = state;
+            }
+
+        });
+    }
+
+    return {
+        init: init
+    };
+})();
---
+++ js/app/main.js
@@ -0,0 +1,47 @@
+﻿window.angularTube.main = (function (service, controller) {
+	var app = angular.module('AngularTube', ["ui.router", "ajoslin.promise-tracker"]);
+
+	function run()
+	{
+		app.run(function () {
+			var tag = document.createElement('script');
+			tag.src = "http://www.youtube.com/iframe_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		});
+	}
+
+	function config()
+	{
+		app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+			$urlRouterProvider.otherwise('/home');
+			$stateProvider
+			  .state('home', {
+				  url: '/home',
+				  templateUrl: 'view/page/home.html',
+				  controller: 'VideosController'
+			  });
+		}])
+		app.config(function ($httpProvider) {
+			$httpProvider.defaults.useXDomain = true;
+			delete $httpProvider.defaults.headers.common['X-Requested-With'];
+		});
+	}
+
+	function init()
+	{
+		if (app != undefined || Object.getOwnPropertyNames(app).length != 0) {
+			run();
+			config();
+			service.init(app);
+			controller.init(app);
+		} else {
+			alert('Angular instance failed.');
+		}
+	}
+
+	return {
+		init:init
+	};
+})(window.angularTube.service,window.angularTube.controller);
+window.angularTube.main.init();
